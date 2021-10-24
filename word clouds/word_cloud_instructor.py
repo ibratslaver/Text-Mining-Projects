436f76bced6aecf32217fb25f5ceaec54706da92
ibratslaver <59898455+ibratslaver@users.noreply.github.com>
2021-10-24 13:49:37 -0400

    Update word_cloud_instructor.py

--- word clouds/word_cloud_instructor.py
+++ word clouds/word_cloud_instructor.py
@@ -11,7 +11,7 @@ import os
 
 stopwords = stopwords.words('english')
 
-# newStopWords = ['instructor', 'Instructor', 'Igor', 'teacher','data', 'analytics', 'well', 'great', 'job', 'kept', 'really', 'everyone', 'time', 'class', 'examples', 'questions','us','understand','easy']
+newStopWords = ['instructor', 'Instructor', 'Igor', 'teacher','data', 'analytics', 'well', 'great', 'job', 'kept', 'really', 'everyone', 'time', 'class', 'examples', 'questions','us','understand','easy']
 # stopwords.extend(newStopWords)
 
 df = pd.read_excel('GA Feedback Categorized.xlsx', sheet_name = "Instructor")
@@ -21,30 +21,30 @@ df['Tokenized_Feedback'] = df['feedback'].apply(word_tokenize)
 
 # CODE TO BREAK FEEDBACK INTO SENTENCES
 
-# for Feedback_ID,Feedback,Date,Recommend in zip(df.ID,df.Tokenized_Feedback, df.Date, df.Recommend):
-#     for Sentence in Feedback:
-#         Sentence = Sentence.replace("'","''")
-#         Feedback_ID = str(Feedback_ID)
-#         Recommend = str(Recommend)
-#         insert_ga_feedback_sentences(Feedback_ID, Sentence, Date, Recommend)
+for Feedback_ID,Feedback,Date,Recommend in zip(df.ID,df.Tokenized_Feedback, df.Date, df.Recommend):
+    for Sentence in Feedback:
+        Sentence = Sentence.replace("'","''")
+        Feedback_ID = str(Feedback_ID)
+        Recommend = str(Recommend)
+        insert_ga_feedback_sentences(Feedback_ID, Sentence, Date, Recommend)
 
-# #VERSION 1: Only Manual Filtering with the stopwords
+#VERSION 1: Only Manual Filtering with the stopwords
 
-# sentence_text = []
+sentence_text = []
 
-# for ind in df.index:
-#     sentence = df['Tokenized_Feedback'][ind]
-#     sentence = [word for word in sentence if word not in ('!','','?',', ', '.',',','(','--',')')]
-#     sentence = [word for word in sentence if word not in stopwords]
-#     sentence = ' '.join(word for word in sentence)
-#     #print(sentence)
-#     sentence_text.append(sentence)
+for ind in df.index:
+    sentence = df['Tokenized_Feedback'][ind]
+    sentence = [word for word in sentence if word not in ('!','','?',', ', '.',',','(','--',')')]
+    sentence = [word for word in sentence if word not in stopwords]
+    sentence = ' '.join(word for word in sentence)
+    #print(sentence)
+    sentence_text.append(sentence)
 
-# combined = ' '.join(sentence_text)
+combined = ' '.join(sentence_text)
 
-# cloud = WordCloud(background_color = "white").generate(combined)
-# plt.imshow(cloud)
-# plt.show()
+cloud = WordCloud(background_color = "white").generate(combined)
+plt.imshow(cloud)
+plt.show()
 
 
 #VERSION 2: Only Manual Filtering with the stopwords
